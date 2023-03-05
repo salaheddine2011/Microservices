099b1bcbf439b8d1558850b753280e6e641dbce5
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-03-05 11:52:13 +0100

    Connecting Currency Exchange Microservice with zipkin (tracing)

--- currency-exchange-service/src/main/java/com/salaheddine/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/salaheddine/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.salaheddine.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,18 +12,25 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     @Autowired
-private CurrencyExchangeRepository repository;
+    private CurrencyExchangeRepository repository;
     @Autowired
     private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyExchange retrieveExchangeValue(@PathVariable String from,@PathVariable String to){
-        CurrencyExchange currencyExchange=repository.findByFromAndTo(from,to);
-        if(currencyExchange == null){
-            throw new RuntimeException("Unable to find data for:"+from+"to"+to);
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+        //INFO [currency-exchange,ba25f511f8591d31568baca00958d7a4,fec97d8c345bf4b2]  3432 --- [nio-8000-exec-1] c.s.m.c.CurrencyExchangeController       : retrieveExchangeValue called with USD to INR
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for:" + from + "to" + to);
         }
-        String port=environment.getProperty("local.server.port");
+        String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironement(port);
-        return currencyExchange ;
+        return currencyExchange;
     }
 }
